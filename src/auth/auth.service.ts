654d57a2a382e11805fc6d380e6213242f48be82
bbakkomm <bbakkomm@gmail.com>
2026-06-05 22:46:28 +0900

    Implement JWT token signing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UsersModel } from '../users/entities/users.entity';
+import { JWT_SECRET } from './const/auth.const';
 
 @Injectable()
 export class AuthService {
@@ -35,5 +37,16 @@ export class AuthService {
    * 2) sub -> id
    * 3) type : access | refresh
    */
-  signToken() {}
+  signToken(user: Pick<UsersModel, 'email' | 'id'>, isRefreshToken: boolean) {
+    const payload = {
+      email: user.email,
+      sub: user.id,
+      type: isRefreshToken ? 'refresh' : 'access',
+    };
+
+    return this.jwtService.sign(payload, {
+      secret: JWT_SECRET,
+      expiresIn: isRefreshToken ? 3600 : 300,
+    });
+  }
 }
